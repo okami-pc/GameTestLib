5f8db226d346feec7374df9481b866fb27087ae7
Phillip Deremer <deremer@orbitlog.com>
2020-09-30 16:28:05 +0200

    add DevConsole

---
+++ DevConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using TicTacToe.NewFolder;
+
+namespace DevConsole {
+    class Program {
+
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            Dictionary<string, object> data = new Dictionary<string, object>() {
+                { "LastName" , "Ich" },
+                { "FirstName", "Du"},
+                { "Address", "Wo?"}
+            };
+
+            Class1 o = new Class1();
+
+            DbColAttr.SetValues<Class1>(o, data);
+
+            Console.ReadKey();
+
+            //using (TicTacToe.Toe myToe = new TicTacToe.Toe()) {
+
+            //    while(!myToe.exit) {
+            //        Point result = myToe.SelectCoordinates();
+            //        myToe.SetField(result, myToe.NextPlayer());
+            //    }
+
+            //    TicTacToe.NewFolder.Class2 obj = new TicTacToe.NewFolder.Class2();
+            //    obj.bla();
+
+            //}
+
+            Console.ReadLine();
+        }
+
+        [System.Diagnostics.DebuggerHidden]
+        static void test() {
+            Console.WriteLine("Click");
+            Console.WriteLine("Click");
+            Console.WriteLine("Click");
+            Console.WriteLine("Click");
+            Console.WriteLine("Click");
+            Console.WriteLine("Click");
+
+        }
+
+        private static void Click_Obj(object sender, EventArgs e) {
+            Console.WriteLine("Click");
+        }
+
+    }
+}
---
+++ TicTacToe/NewFolder/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace TicTacToe.NewFolder {
+
+    public class Class1 {
+
+        [DbColAttr("LastName")]
+        public string name { get; set; }
+
+        [DbColAttr("FirstName")]
+        public string fname { get; set; }
+
+        [DbColAttr("Address")]
+        public string street { get; set; }
+
+    }
+
+    public class DbColAttr : Attribute {
+        public string ColumnName { get; set; }
+        public DbColAttr(string colName) : base() {
+            ColumnName = colName;
+        }
+
+        public static void SetValues<T>(T o, Dictionary<string, object> data) {
+            Type oType = o.GetType();
+
+            foreach(PropertyInfo pi in oType.GetProperties()) {
+                DbColAttr? ca = pi.GetCustomAttribute<DbColAttr>();
+                if (ca != null) {
+                    pi.SetValue(o, data[ca?.ColumnName]);
+                }
+            }
+        }
+    }
+
+}
